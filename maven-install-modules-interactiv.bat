0bca302ae47f57aa2839931ee00b659342bfafab
brabenetz <brabenetz@gmx.net>
2012-11-19 01:07:57 +0100

    Update build Script to install or deploy Modules interactiv

--- maven-install-modules-interactiv.bat
+++ maven-install-modules-interactiv.bat
@@ -3,5 +3,5 @@ call set ANT_OPTS=-Xms128m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m
 call echo ANT_OPTS: %ANT_OPTS%
 call set MAVEN_OPTS=-Xms128m -Xmx1024m -XX:PermSize=128m -XX:MaxPermSize=512m
 call echo MAVEN_OPTS: %MAVEN_OPTS%
-call ant maven-install-init-all
+call ant maven-install-modules-interactiv -Dexternaldirectories=%userprofile%/git/modules-v8-brabenetz
 pause
